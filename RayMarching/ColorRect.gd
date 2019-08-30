0eed71a57549496612be9949e7945cf0e17cdb8d
PLUkraine <plukraine001@gmail.com>
2019-08-30 08:58:15 -0500

    Add Specular light component

--- RayMarching/ColorRect.gd
+++ RayMarching/ColorRect.gd
@@ -2,12 +2,13 @@ extends ColorRect
 
 onready var start_time = OS.get_ticks_msec()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	self.material.set_shader_param("cameraPos", Vector3(0, 0, 10.0))
-	
+    
 func _process(delta):
-	var time_since_start = (OS.get_ticks_msec() - start_time) / 1000.0
-	
-	self.material.set_shader_param("cameraPos", 
-		Vector3(0, 0, 5.0 + 2.0 * cos(time_since_start)))
+    var time = (OS.get_ticks_msec() - start_time) / 1000.0
+    self.material.set_shader_param("lightPos",
+        Vector3(2.0*cos(time), 5.0, 2.0*sin(time)))
+    
+#    self.material.set_shader_param("fov", 
+#        15.0 * cos(time_since_start) + 45.0)
+#    self.material.set_shader_param("cameraPos", 
+#        Vector3(2.0 * cos(time_since_start), 0, 5.0))
